db52c5e27c565944e0cb5e7dd87e9a4c1b185e4c
Sasha1358 <0551553733z@gmail.com>
2023-10-19 13:06:34 +0300

    Добавьте файлы проекта.

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = A(1, 2, 1);
+			Assert::AreEqual(t, -0.6);
+		}
+	};
+}
